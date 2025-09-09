b4502fae56fc6c6a7a519b8d88197afdffc2844f
opilane <opilane@HP-CZC0359NN6>
2025-09-09 10:00:33 +0300

    a

---
+++ DatabaseTask/DatabaseTask.Data/UserDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseTask.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseTask.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) 
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
